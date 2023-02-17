25eb92cf838cf0397ef96b0bed6fb4c9efa4802b
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-17 17:09:55 -0500

    modified views.py + created urls.py in music app

--- music/views.py
+++ music/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def song_list(request):
+
+    return Response('ok')
