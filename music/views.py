a96c6e2569d6cb8c9aefd1a1166d5f4202f4fbb5
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-17 17:48:44 -0500

    modified views.py in music

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import MusicSerializer
+from .models import Song
 
 @api_view(['GET'])
 def song_list(request):
 
-    return Response('ok')
+    music = Song.objects.all()
+
+    serializer = MusicSerializer(music, many=True)
+
+    return Response(serializer.data)
