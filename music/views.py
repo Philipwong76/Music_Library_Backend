5e6eeb566752e053b97c05dfc03f6fd1afdb1e5d
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-17 20:09:03 -0500

    updated view.py

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,12 +18,16 @@ def song_list(request):
         serializer.save()
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def song_detail(request, pk):
-    try:
-        song = Song.objects.get(pk=pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
         serializer = MusicSerializer(song);
         return Response(serializer.data)
-        
-    except Song.DoesNotExist:    
-        return Response(status=status.HTTP_404_NOT_FOUND);
+    elif request.method == 'PUT':
+        song = get_object_or_404(Song, pk=pk)
+        serializer = MusicSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
